6f8b5757d70eb3748375c48b8c84f933d5887e94
Ranith184 <ranithyadav1804@gmail.com>
2026-06-25 17:17:12 +0530

    feat: update menu database and butter config with local image assets

--- TheOh/theoh-breakfast/src/data/butters.js
+++ TheOh/theoh-breakfast/src/data/butters.js
@@ -1,9 +1,9 @@
 import { IMAGE_ASSETS } from './imageAssets';
 
 export const butters = [
-  { id: "bu1", name: "Almond Butter (Creamy Crunchy)", price: 40, tags: ["Healthy Fats"], image: IMAGE_ASSETS.addons.almondButter },
+  { id: "bu1", name: "Almond Butter (Creamy Crunchy)", price: 40, tags: ["Healthy Fats"], image: "public/assets/images/almond.png" },
   { id: "bu2", name: "Dark Chocolate Peanut Butter", price: 45, tags: ["Healthy Fats", "High Protein"], image: IMAGE_ASSETS.addons.darkChocolatePeanutButter },
   { id: "bu3", name: "Peanut Butter", price: 35, tags: ["Healthy Fats", "High Protein"], image: IMAGE_ASSETS.addons.peanutButter },
   { id: "bu4", name: "Honey", price: 20, tags: [], image: IMAGE_ASSETS.addons.honey },
-  { id: "bu5", name: "Cinnamon", price: 15, tags: [], image: IMAGE_ASSETS.addons.cinnamon }
+  //{ id: "bu5", name: "Cinnamon", price: 15, tags: [], image: IMAGE_ASSETS.addons.cinnamon }
 ];
